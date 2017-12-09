739835eb36b512235c486daf7f5b04ccc334bfaf
chickinnick <chickinnick@ya.ru>
2017-12-09 21:45:42 +0200

    initial commit

---
+++ app/src/main/java/com/chickinnick/windspeedmeasurement/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chickinnick.windspeedmeasurement
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
